bc8dce741600a8ae1c966736b5ca36fb86c2cf3a
Brandon Pickett <pickettbd@gmail.com>
2014-11-26 21:47:38 -0500

    pretty html

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var MongoClient = require('mongodb').MongoClient
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.locals.pretty = true;
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
