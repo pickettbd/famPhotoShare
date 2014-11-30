183e1ee33f63bd1d3b022331e9dd9a0f21764a40
Ammon Gruwell <gruwella@gmail.com>
2014-11-30 01:56:10 -0700

    Can now add user info into database from sign-up page, still can't check user credentials from login page

--- routes/routes.js
+++ routes/routes.js
@@ -44,11 +44,68 @@ router.get('/', function(req, res) {
 /* GET photolist page. */
 router.get('/photolist', function(req, res) {
     var db = req.db;
-    var collection = db.get('photos');
+    var collection = db.get('users');
     collection.find({},{},function(e,docs){
         res.render('photolist', {
             "photolist" : docs
         });
+    });
+});
+
+/*GET to check user credentials*/
+router.post('/checkuser', function(req, res) {
+    
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var username = req.body.username;
+    var pass = req.body.password;
+
+    // Set our collection
+    var collection = db.get('users');
+
+    collection.find({userName:username},{},function(err,items){
+        //This always returns true whether it is in the database or not
+        if(items != null){
+            res.location("menu");
+            res.redirect("menu");
+        };
+    });
+
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var confimrpass = req.body.confirmpass;
+
+    // Set our collection
+    var collection = db.get('users');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : email,
+	"password" : password
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("menu");
+            // And forward to success page
+            res.redirect("menu");
+        }
     });
 });
 
