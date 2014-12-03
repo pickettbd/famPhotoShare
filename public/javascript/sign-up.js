f29fd07ee995e35f84efa767282bccc5b32a8053
Ammon Gruwell <gruwella@gmail.com>
2014-12-03 12:22:38 -0700

    added javascript directory

---
+++ public/javascript/sign-up.js
@@ -0,0 +1,10 @@
+
+
+function saveUserData(){
+	var userName = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	var email = document.getElementById('email').value;
+	var confirmPass = document.getElementById('confirmpass').value;
+
+	
+}
