ae0b0d4e01a9d1d3c7e2563a9654bc4aa20e30e9
John Duce <john.d.duce@gmail.com>
2014-12-03 22:04:37 -0700

    ability to select and deselect images for download

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,23 +1,40 @@
 (function() {
 
-	var app = angular.module('thumbDisplayer', [ ]);
+	var app = angular.module('thumbDisplayer', [])
 
 	app.controller('ThumbController', function(){
 		this.thumbs = images;
 	});
 
+	app.controller('PanelController', function(){
+		this.selectedPhotos = [];
+
+		this.selectPhoto = function(selectPhoto) {
+			var index = this.selectedPhotos.indexOf(selectPhoto)
+			if(index === -1) {
+				this.selectedPhotos.push(selectPhoto);
+				return;
+			}
+			this.selectedPhotos.splice(index, 1);
+		};
+
+		this.isSelected = function(checkPhoto) {
+			return -1 != this.selectedPhotos.indexOf(checkPhoto);
+		};
+	});
+
 	var images = [
-		"/images/temp_images/1.jpg",
-		"/images/temp_images/2.jpg",
-		"/images/temp_images/3.jpg",
-		"/images/temp_images/4.jpg",
-		"/images/temp_images/5.jpg",
-		"/images/temp_images/6.jpg",
-		"/images/temp_images/7.jpg",
-		"/images/temp_images/8.jpg",
-		"/images/temp_images/9.jpg",
-		"/images/temp_images/10.jpg",
-		"/images/temp_images/11.jpg",
+		{ url: "/images/temp_images/1.jpg", id: "1" },
+		{ url: "/images/temp_images/2.jpg", id: "2" },
+		{ url: "/images/temp_images/3.jpg", id: "3" },
+		{ url: "/images/temp_images/4.jpg", id: "4" },
+		{ url: "/images/temp_images/5.jpg", id: "5" },
+		{ url: "/images/temp_images/6.jpg", id: "6" },
+		{ url: "/images/temp_images/7.jpg", id: "7" },
+		{ url: "/images/temp_images/8.jpg", id: "8" },
+		{ url: "/images/temp_images/9.jpg", id: "9" },
+		{ url: "/images/temp_images/10.jpg", id: "10" },
+		{ url: "/images/temp_images/11.jpg", id: "11" }
 	];
 
 })();
