1666a99e6af1cec9f019a0c4bc18ddaf18d6a383
Ammon Gruwell <gruwella@gmail.com>
2014-12-04 11:50:26 -0700

    modified db connection

--- schemas/user.js
+++ schemas/user.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/memshare');
+var db = mongoose.connection;
 var db = mongoose.createConnection('mongodb://localhost/memshare');
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback () {
-    console.log("connected successfully");
+    console.log("connected successfully in user.js");
 }); 
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
