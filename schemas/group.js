8677bd59f1da03d19fe4b96b61915775a97d3e34
Brandon Pickett <pickettbd@gmail.com>
2014-12-04 14:57:44 -0700

    fixed group/event schemas

--- schemas/group.js
+++ schemas/group.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var groupSchema = new Schema({
     name: String,    
-    events: [{type: Schema.ObjectId, ref: 'Event'}],
+    //events: [{type: Schema.ObjectId, ref: 'Event'}],
+    events: [ { name: String, photos: [ String ] } ],
     users: [String]
 });
 
