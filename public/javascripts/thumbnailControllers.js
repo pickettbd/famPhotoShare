f7d279d93251ff1e59b849eadc267d9c7334ce2f
John Duce <john.d.duce@gmail.com>
2014-12-06 18:02:04 -0700

    everything on download page works except for actually downloading photos

--- public/javascripts/thumbnailControllers.js
+++ public/javascripts/thumbnailControllers.js
@@ -1,5 +1,54 @@
 (function() {
 
+	angular.module('app').controller('ListController', function($scope, $http) {
+		var initial = true;
+		var group = "";
+
+		$http.get('http://localhost/api/users/whoami').then(function(usernameRes) {
+
+ 			var username = usernameRes.data;
+
+			$http.get('http://localhost/api/users/' + username + '/groups').then(function(groupsRes) {
+
+				$scope.groups = groupsRes.data;
+
+			}, function(err) {
+					console.error('ERR', err);
+			});
+ 		}, function(err) {
+				console.error('ERR', err);
+		});
+
+		this.getEvents = function() {
+			var ddl = document.getElementById("groupsddl");
+			group = ddl.options[ddl.selectedIndex].value;
+			$http.get('http://localhost/api/groups/' + group + '/events').then(function(eventsRes) {
+				$scope.events = eventsRes.data;
+			}, function(err) {
+					console.error('ERR', err);
+			});
+		};
+
+		this.getThumbs = function() {
+			var ddl = document.getElementById("eventsddl");
+			eventname = ddl.options[ddl.selectedIndex].value;
+			$http.get('http://localhost/api/groups/' + group + '/events/' + eventname + '/thumbs').then(function(thumbsRes) {
+				$scope.thumbs = thumbsRes.data;
+			}, function(err) {
+					console.error('ERR', err);
+			});
+		};
+
+		this.hasThumbs = function() {
+			if(initial === true) {
+				initial = false;
+				return true;
+			}
+			return $scope.thumbs.length > 0;
+		};
+
+	});
+
 	angular.module('app').controller('ThumbController', function(){
 		this.selectedPhotos = [];
 
@@ -15,46 +64,6 @@
 		this.isSelected = function(checkPhoto) {
 			return -1 != this.selectedPhotos.indexOf(checkPhoto);
 		};
-	});
-
-	angular.module('app').controller('ListController', function($scope, $http) {
-		var group = "";
-
-		$http.get('http://104.236.25.185/api/users/whoami').then(function(usernameRes) {
-
- 			var username = usernameRes.data;
-
-			$http.get('http://104.236.25.185/api/users/' + username + '/groups').then(function(groupsRes) {
-
-				$scope.groups = groupsRes.data;
-
-			}, function(err) {
-					console.error('ERR', err);
-			});
- 		}, function(err) {
-				console.error('ERR', err);
-		});
-
-		this.getEvents = function() {
-			var ddl = document.getElementById("groupsddl");
-			group = ddl.options[ddl.selectedIndex].value;
-			$http.get('http://104.236.25.185/api/groups/' + group + '/events').then(function(eventsRes) {
-				$scope.events = eventsRes.data;
-			}, function(err) {
-					console.error('ERR', err);
-			});
-		};
-
-		this.getThumbs = function() {
-			var ddl = document.getElementById("eventsddl");
-			eventname = ddl.options[ddl.selectedIndex].value;
-			$http.get('http://104.236.25.185/api/groups/' + group + '/events/' + eventname + '/thumbs').then(function(thumbsRes) {
-				$scope.thumbAddresses = thumbsRes.data;
-			}, function(err) {
-					console.error('ERR', err);
-			});
-		};
-
 	});
 
 })();
