c1acd6ed815751f2448d30baf5cc80df144214b0
John Duce <john.d.duce@gmail.com>
2014-12-06 19:55:31 -0700

    switch urls from localhost to server ip address

--- public/javascripts/thumbnailControllers.js
+++ public/javascripts/thumbnailControllers.js
@@ -8,10 +8,10 @@
 		$scope.eventname = '';
 		$scope.events = [];
 
-		var url = 'http://localhost/api/users/whoami';
+		var url = 'http://104.236.25.185/api/users/whoami';
 		$http.get(url).then(function(usernameRes) {
  			var username = usernameRes.data;
-			url = 'http://localhost/api/users/' + username + '/groups';
+			url = 'http://104.236.25.185/api/users/' + username + '/groups';
 			$http.get(url).then(function(groupsRes) {
 				$scope.groups = groupsRes.data;
 			}, function(err) {
@@ -23,11 +23,11 @@
 
 		this.getEvents = function(groupIn) {
 			group = groupIn;
-			url = 'http://localhost/api/groups/' + group + '/events';
+			url = 'http://104.236.25.185/api/groups/' + group + '/events';
 			$http.get(url).then(function(eventsRes) {
 				$scope.events = eventsRes.data;
 				$scope.eventname = eventsRes.data[0];
-				url = 'http://localhost/api/groups/' + group + '/events/' + $scope.eventname + '/thumbs';
+				url = 'http://104.236.25.185/api/groups/' + group + '/events/' + $scope.eventname + '/thumbs';
 				$http.get(url).then(function(thumbsRes) {
 					$scope.thumbs = thumbsRes.data;
 				}, function(err) {
@@ -40,7 +40,7 @@
 
 		this.getThumbs = function(eventnameIn) {
 			$scope.eventname = eventnameIn;
-			url = 'http://localhost/api/groups/' + group + '/events/' + $scope.eventname + '/thumbs';
+			url = 'http://104.236.25.185/api/groups/' + group + '/events/' + $scope.eventname + '/thumbs';
 			$http.get(url).then(function(thumbsRes) {
 				$scope.thumbs = thumbsRes.data;
 			}, function(err) {
